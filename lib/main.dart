ec126d4bd99cb692701731fcdc1383999ed307e3
KenzieL16 <anandabagas722@gmail.com>
2024-02-15 13:36:30 +0700

    fetch home and detail

--- lib/main.dart
+++ lib/main.dart
@@ -237,7 +237,7 @@ class CardTemplate extends StatelessWidget {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => DetailCatatanPage(),
+                builder: (context) => DetailCatatanPage(slug: catatanData['slug']),
               ),
             );
           },
