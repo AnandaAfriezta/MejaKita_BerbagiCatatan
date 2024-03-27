d1dd567acd53dc64731cc80dff986d2fac04599d
KenzieL16 <anandabagas722@gmail.com>
2024-03-27 10:55:01 +0700

    API Constant & Class Data

--- lib/edit_catatan.dart
+++ lib/edit_catatan.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:berbagi_catatan/widget/header.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'detail_catatan.dart';
+import 'catatanData.dart';
 import 'main.dart';
 
 class EditCatatanPage extends StatefulWidget {
